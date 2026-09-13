e28d979fdee1b146603dd736159a25c864e3b805
SakshamSaharan <saharansaksham023@gmail.com>
2026-09-13 23:52:19 +0530

    Time: 10 ms (30.07%), Space: 12.3 MB (62.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -11,31 +11,9 @@ public:
         mp['M'] = 1000;
         int sum = 0;
         for(int i=0;i<s.size();i++){
-            auto it = mp.find(s[i]);
-            sum += it->second;
-            if(it->first == 'V' || it->first == 'X'){
-                if(i > 0){
-                    auto it2 = mp.find(s[i-1]);
-                    if(it2->first == 'I'){
-                        sum-= 2*it2->second;
-                    }
-                }
-            }
-            else if(it->first == 'L' || it->first == 'C'){
-                if(i > 0){
-                    auto it2 = mp.find(s[i-1]);
-                    if(it2->first == 'X'){
-                        sum-= 2*it2->second;
-                    }
-                }
-            }
-            else if(it->first == 'D' || it->first == 'M'){
-                if(i > 0){
-                    auto it2 = mp.find(s[i-1]);
-                    if(it2->first == 'C'){
-                        sum-= 2*it2->second;
-                    }
-                }
+            sum += mp[s[i]];
+            if(i>0 && mp[s[i]] > mp[s[i-1]]){
+                sum -= 2*mp[s[i-1]];
             }
         }
         return sum;
