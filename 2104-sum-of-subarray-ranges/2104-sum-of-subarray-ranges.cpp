7b9f217ce6e9999175c01d07cd9cfddfcab74281
SakshamSaharan <saharansaksham023@gmail.com>
2026-09-15 19:31:09 +0530

    Time: 7 ms (41.46%), Space: 15.7 MB (52.39%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,18 +1,93 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long sum = 0;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int maxi = nums[i];
-            int mini = nums[i];
-            for(int j=i;j<n;j++){
-                maxi = max(maxi,nums[j]);
-                mini = min(mini,nums[j]);
-                int range = maxi - mini;
-                sum += range;
+        stack<int>s;
+        int n = nums.size(); 
+        vector<int>nse(n,n);
+        s.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            int curr = nums[i];
+            while(! s.empty() && curr < nums[s.top()]){
+                s.pop();
             }
+            if(s.empty()){
+                nse[i] = n;
+            }
+            else{
+                nse[i] = s.top();
+            }
+            s.push(i);
         }
-        return sum;
+        while(! s.empty()){
+            s.pop();
+        }
+        vector<int>pse(n,-1);
+        s.push(0);
+        for(int i=1;i<n;i++){
+            int curr = nums[i];
+            while(! s.empty() && curr <= nums[s.top()]){
+                s.pop();
+            }
+            if(s.empty()){
+                pse[i] = -1;
+            }
+            else{
+                pse[i] = s.top();
+            }
+            s.push(i);
+        }
+        long long minsum = 0;
+        for(int i=0;i<n;i++){
+            int left = i - pse[i];
+            int right = nse[i] - i;
+            long long freq = left*right;
+            long long val = freq*nums[i];
+            minsum += val;
+        }
+        while(! s.empty()){
+            s.pop();
+        }
+        vector<int>nge(n,n);
+        s.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            int curr = nums[i];
+            while(! s.empty() && curr > nums[s.top()]){
+                s.pop();
+            }
+            if(s.empty()){
+                nge[i] = n;
+            }
+            else{
+                nge[i] = s.top();
+            }
+            s.push(i);
+        }
+        while(! s.empty()){
+            s.pop();
+        }
+        vector<int>pge(n,-1);
+        s.push(0);
+        for(int i=1;i<n;i++){
+            int curr = nums[i];
+            while(! s.empty() && curr >= nums[s.top()]){
+                s.pop();
+            }
+            if(s.empty()){
+                pge[i] = -1;
+            }
+            else{
+                pge[i] = s.top();
+            }
+            s.push(i);
+        }
+        long long maxsum = 0;
+        for(int i=0;i<n;i++){
+            int left = i - pge[i];
+            int right = nge[i] - i;
+            long long freq = left*right;
+            long long val = freq*nums[i];
+            maxsum += val;
+        }
+        return maxsum-minsum;
     }
 };
